8adffeefb58ed264b793cb17c7aa4dfacb8e0542
Jason Rogers <jason@ascendiv.com>
2015-06-26 15:58:37 -0400

    Initial barebones support for Google Maps/geolocation

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,5 +1,6 @@
 angular.module('MedwatchApp', [
     'AppRouter',
-    'HomeCtrl'
+    'HomeCtrl',
+    'RecallMap'
 ]);
 
---
+++ src/scripts/app/directives/RecallMap.js
@@ -0,0 +1,38 @@
+angular.module('RecallMap', [])
+    .directive('recallmap', ['$window', function($window) {
+        'use strict';
+
+        return {
+            restrict: 'E',
+            template: '<div><div class="hidden">Sorry, but your browser does not support geolocation features.</div></div>',
+            replace: true,
+            link: function(scope, elem, attrs) {
+                if (!('geolocation' in $window.navigator)) {
+                    return elem.children().removeClass('hidden');
+                }
+
+                $window.navigator.geolocation.getCurrentPosition(showMap, showMapWithoutLocation);
+
+                function showMap(position) {
+                    scope.$apply(function() {
+                        var mapOptions = {
+                            zoom: 15,
+                            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+                        };
+                        var map = new google.maps.Map(document.getElementById(attrs.id), mapOptions);
+                        var marker = new google.maps.Marker({
+                            position: mapOptions.center,
+                            map: map,
+                            title: 'You are here'
+                        });
+                        marker.setMap(map);
+                    });
+                }
+
+                function showMapWithoutLocation() {
+                    // set position with center of US in focus
+                    showMap({ coords: { latitude: 37.09024, longitude: -100.712891 } });
+                }
+            }
+        };
+    }]);
