aef65503eaf108d49c17c932d727a5bff9bf2adf
Nathan Evans <github@talkswithnumbers.com>
2015-06-29 17:54:32 -0700

    #6 - added less compile and gulp inject

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
+var path = require('path');
+var less = require('gulp-less');
+var inject = require('gulp-inject');
 var webserver = require('gulp-webserver');
 var jshint = require('gulp-jshint');
 var rimraf = require('rimraf');
@@ -16,7 +19,6 @@ var config = {
             'src/**/*',
             'bower_components/USPTOPatternLibrary/bower_components/**/*',
             'bower_components/USPTOPatternLibrary/usptostrap/**/*',
-            'index.html',
             '.nojekyll'
         ],
         build: 'dist',
@@ -35,6 +37,55 @@ gulp.task('lint', function () {
         .pipe(jshint.reporter('default'));
 });
 
+gulp.task('less', function () {
+    return gulp.src('src/styles/**/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('dist/src/css'));
+});
+
+/*
+ * Clean out the build directory so we don't have any excess junk
+ */
+gulp.task('clean', function (cb) {
+    rimraf(config.src.build, cb);
+});
+
+/**
+ * Replaces development-time less compilation with compiled css for distribution.
+ */
+gulp.task('inject', function () {
+    var target = gulp.src('index.html');
+    var sources = gulp.src(['dist/src/css/styles.css'], {
+        read: false
+    });
+    //transform removes our output prefix so it is correctly relative within dist/index.html
+    var transform = function (filepath) {
+        arguments[0] = filepath.replace('/dist/', '');
+        return inject.transform.apply(inject.transform, arguments);
+
+    };
+
+    return target.pipe(inject(sources, { transform: transform }))
+        .pipe(gulp.dest('dist'));
+});
+
+/*
+ * Copy static content into a single point for deployment, without the extra cruft.
+ */
+gulp.task('site', function () {
+    return gulp.src(config.src.siteFiles, { 'base': '.' }).pipe(gulp.dest(config.src.build));
+});
+
+gulp.task('build', function (cb) {
+    return runSequence(
+        'less',
+        'inject',
+        'site',
+        cb);
+});
+
 /*
  * Live-reload server to make the app available (localhost:8000) and auto-refresh when files change.
  */
@@ -44,7 +95,7 @@ gulp.task('watch', function() {
             path: '/18f-RFQ993471-POOL1',
             livereload: true,
             defaultFile: 'index.html',
-            open: true
+            open: false
         }));
 });
 
@@ -58,18 +109,14 @@ gulp.task('serve', function() {
         }));
 });
 
-/*
- * Clean out the build directory so we don't have any excess junk
- */
-gulp.task('clean', function (cb) {
-    rimraf(config.src.build, cb);
-});
-
-/*
- * Get all that stuff into a single point for deployment, without the extra cruft.
- */
-gulp.task('build', function () {
-    return gulp.src(config.src.siteFiles, { 'base': '.' }).pipe(gulp.dest(config.src.build));
+gulp.task('serve-dist', function() {
+    gulp.src(config.src.build)
+        .pipe(webserver({
+            path: '/18f-RFQ993471-POOL1',
+            livereload: false,
+            defaultFile: 'index.html',
+            open: false
+        }));
 });
 
 /*
