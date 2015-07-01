1e0cd346028011ee5671f003c11b4518a92703fd
Bingles <brian.ingles@gmail.com>
2015-07-01 13:38:07 -0400

    map styling

--- src/scripts/app/directives/RecallMap.js
+++ src/scripts/app/directives/RecallMap.js
@@ -160,8 +160,13 @@ angular.module('RecallMap', [])
                 function showMap(position, zoom) {
                     // scope.$apply(function() {
                         var mapOptions = {
-                            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+                            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                  
+                    // Map Styles
+                        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"},{"color":"#052366"},{"saturation":"-70"},{"lightness":"85"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":"-53"},{"weight":"1.00"},{"gamma":"0.98"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"on"}]},{"featureType":"road","elementType":"geometry","stylers":[{"saturation":"-18"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#57677a"},{"visibility":"on"}]}]
                         };
+
+
                         var map = new google.maps.Map(document.getElementById(attrs.id), mapOptions);
                         var circle = new google.maps.Circle({ // circle is used for setting the zoom level
                             center: mapOptions.center,
