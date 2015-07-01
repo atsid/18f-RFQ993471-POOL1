95f24dd8dda891f538a2d24f3d23cc39febef21f
Bingles <brian.ingles@gmail.com>
2015-07-01 14:20:51 -0400

    Update 2015-07-01T18:20:46.947Z

--- src/scripts/app/directives/RecallMap.js
+++ src/scripts/app/directives/RecallMap.js
@@ -163,7 +163,107 @@ angular.module('RecallMap', [])
                             center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
                   
                     // Map Styles
-                        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"},{"color":"#052366"},{"saturation":"-70"},{"lightness":"85"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":"-53"},{"weight":"1.00"},{"gamma":"0.98"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"on"}]},{"featureType":"road","elementType":"geometry","stylers":[{"saturation":"-18"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#57677a"},{"visibility":"on"}]}]
+                        styles: [
+                                            {
+                                                "featureType": "administrative.country",
+                                                "elementType": "all",
+                                                "stylers": [
+                                                    {
+                                                        "visibility": "on"
+                                                    },
+                                                    {
+                                                        "color": "#14c7c7"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "administrative.country",
+                                                "elementType": "geometry",
+                                                "stylers": [
+                                                    {
+                                                        "visibility": "on"
+                                                    },
+                                                    {
+                                                        "color": "#617995"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "administrative.country",
+                                                "elementType": "labels.text",
+                                                "stylers": [
+                                                    {
+                                                        "color": "#212f4f"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "administrative.country",
+                                                "elementType": "labels.text.stroke",
+                                                "stylers": [
+                                                    {
+                                                        "visibility": "off"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "landscape.natural.landcover",
+                                                "elementType": "geometry",
+                                                "stylers": [
+                                                    {
+                                                        "hue": "#ff0000"
+                                                    },
+                                                    {
+                                                        "lightness": "25"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "landscape.natural.landcover",
+                                                "elementType": "geometry.fill",
+                                                "stylers": [
+                                                    {
+                                                        "hue": "#00ffd5"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "landscape.natural.landcover",
+                                                "elementType": "labels.text.stroke",
+                                                "stylers": [
+                                                    {
+                                                        "visibility": "simplified"
+                                                    },
+                                                    {
+                                                        "hue": "#ff0000"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "landscape.natural.terrain",
+                                                "elementType": "geometry.fill",
+                                                "stylers": [
+                                                    {
+                                                        "visibility": "on"
+                                                    },
+                                                    {
+                                                        "color": "#baca7c"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "featureType": "water",
+                                                "elementType": "all",
+                                                "stylers": [
+                                                    {
+                                                        "visibility": "on"
+                                                    },
+                                                    {
+                                                        "color": "#87a6bc"
+                                                    }
+                                                ]
+                                            }
+                                        ]
                         };
 
 
