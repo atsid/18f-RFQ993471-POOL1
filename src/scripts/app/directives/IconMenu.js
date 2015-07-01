a03b49828f4f574a9b61a04a9e4ec75cf77d8e18
Jason Rogers <jason@ascendiv.com>
2015-07-01 12:04:08 -0400

    Update 2015-07-01T16:04:06.395Z

--- src/scripts/app/directives/IconMenu.js
+++ src/scripts/app/directives/IconMenu.js
@@ -13,6 +13,7 @@ angular.module('IconMenu', [])
                     $scope.activeButton = angular.element($event.target).attr('id');
                     EventBusService.publish('toggleDetails', value);
                 };
+                $scope.activeButton = 'recall-btn'; //default
             }]
         };
     });
