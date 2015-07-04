2a8e124a5414cbb1207d9ac3f561ab9158025614
Jason Rogers <jason@ascendiv.com>
2015-07-04 09:28:01 -0400

    Update 2015-07-04T13:27:59.030Z

--- src/scripts/app/directives/RecallMap.js
+++ src/scripts/app/directives/RecallMap.js
@@ -263,7 +263,7 @@ angular.module('RecallMap', ['EventBus'])
                         // NOTE: This also seems to help the map display more reliably on Android.
                         $timeout(function() {
                             map.setZoom(map.getZoom() + 1);
-                        }, 400);
+                        }, 1000);
                     // });
                 }
 
