a6aa4f4534c78b9e97eeb0114625a92ae5783dac
Jason Rogers <jason@ascendiv.com>
2015-07-06 15:36:53 -0400

    Update 2015-07-06T19:36:50.661Z

--- src/scripts/app/controllers/HomeCtrl.js
+++ src/scripts/app/controllers/HomeCtrl.js
@@ -1,6 +1,12 @@
-angular.module('HomeCtrl', ['Search'])
-    .controller('HomeController', [function() {
-            'use strict';
-            // placeholder
+angular.module('HomeCtrl', ['EventBus'])
+    .controller('HomeController', ['$scope', 'EventBusService', function($scope, EventBusService) {
+        'use strict';
+
+        var vm = this;
+        vm.showLoadingSpinner = false;
+
+        EventBusService.subscribe($scope, 'toggleLoadingSpinner', function(shouldShow) {
+            vm.showLoadingSpinner = shouldShow;
+        });
     }]);
 
